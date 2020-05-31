c67370ed76dd32263cfa179e4a9c566dc8413ed5
run-after <run.after@protonmail.com>
2020-05-31 11:25:13 -0500

    Finish project

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,21 @@
 class StaticPagesController < ApplicationController
+  require 'flickraw'
+
   def home
+    @user = params[:user_id]
+    flickr = FlickRaw::Flickr.new
+    @photos = flickr.people.getPublicPhotos(user_id: @user) unless @user.nil?
+
+    unless @photos.nil?
+      @photos.map do |pic| 
+         @title = "#{pic.title}"
+         @url = url_generate(pic)
+       end 
+    end 
+
   end
+  private
+    def url_generate(pic)
+      "https://farm#{pic.farm}.staticflickr.com/#{pic.server}/#{pic.id}_#{pic.secret}.jpg"
+    end
 end
---
+++ config/initializers/flickraw.rb
@@ -0,0 +1,2 @@
+FlickRaw.api_key = ENV['flickraw_key']
+FlickRaw.shared_secret = ENV['flickraw_secret']
